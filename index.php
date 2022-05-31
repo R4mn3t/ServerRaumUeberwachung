5f36acd3d4bb273ca5bf1eb1ebe61b92e2f184a4
R4mn3t <knystockmarten@gmail.com>
2022-05-31 20:47:36 +0200

    script löschen

--- index.php
+++ index.php
@@ -77,19 +77,6 @@
         </div>
     </div>
 </section>
-
-<script>
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn = document.querySelector(".sidebarBtn");
-    sidebarBtn.onclick = function () {
-        sidebar.classList.toggle("active");
-        if (sidebar.classList.contains("active")) {
-            sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-        } else
-            sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
-</script>
-
 </body>
 </html>
 
